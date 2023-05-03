1f4b32c73d2fbf42c1e381619923d441283a0668
wendel fleming <wendel@gmail.com>
2023-05-02 21:06:28 -0700

    Added bootstrap and axios to default generated Vue project.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import { createApp } from 'vue'
+import axios from 'axios';
+
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
+
+const app = createApp(App);
+
+axios.defaults.withCredentials = false;
+axios.defaults.baseURL = 'http://localhost:5000';
+
+app.use(router);
+app.mount("#app");
